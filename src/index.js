5b1fddb72abb5f3c0d64a271a4b1ffa538a6be85
Nicolay Hvidsten <nicohvi@gmail.com>
2017-01-21 14:20:36 +0100

    init :bear:

---
+++ src/index.js
@@ -0,0 +1,80 @@
+function slice (node) {
+  return node instanceof NodeList
+    ? [].slice.call(node)
+    : new Array(node);
+}
+
+var methods = {
+  addClass (name) {
+    this.forEach(el => el.classList.add(name));
+  },
+
+  data (selector) {
+    return this[0].dataset[selector];
+  },
+
+  find (selector) {
+    return wrap(this[0].querySelectorAll(selector));
+  },
+
+  hasClass (name) {
+    return this[0].classList.contains(name);
+  },
+
+  on (event, handler) {
+    this.forEach(el => el.addEventListener(event, handler));
+  },
+
+  offset () {
+    const body = document.body.getBoundingClientRect();
+    const bounds = this[0].getBoundingClientRect();
+    return { 
+      top: bounds.top - body.top, 
+      left: bounds.left - body.left
+    };
+  },
+
+  removeClass (name) {
+    this[0].classList.remove(name);
+  },
+
+  toggleClass (name) {
+    this[0].classList.toggle(name);
+  },
+
+  width () {
+    return this[0].clientWidth;
+  }
+  
+};
+
+function notEmpty (fn) {
+  return function () {
+    if(this.length === 0) return this;
+    return fn.apply(this, arguments);
+  } 
+}
+
+function decorate (wrapper) {
+  for(let method in methods) {
+    wrapper[method] = notEmpty(methods[method]);
+  }
+  return wrapper;
+}
+
+function wrap (el) {
+  let wrapper = Object.create(Array.prototype);
+  slice(el).forEach(node => wrapper.push(node));
+  return decorate(wrapper);
+}
+
+module.exports = function (query) {
+  if(query[0] === '#') {
+       return wrap(document.getElementById(query.substring(1)));
+  }
+  if (query instanceof HTMLElement) {
+    return wrap(query);
+  }
+  return wrap(document.querySelectorAll(query));
+}
+
